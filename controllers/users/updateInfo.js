d0cc70f12ac25f6601a6e1442fd9dcd7bf35d078
IhorKulinskyi <110032104+IhorKulinskyi@users.noreply.github.com>
2023-09-16 20:43:48 +0300

    update info fix

--- controllers/users/updateInfo.js
+++ controllers/users/updateInfo.js
@@ -10,24 +10,32 @@ const updateInfo = async (req, res) => {
     { new: true }
   );
 
-  const { desiredtWeight, height, birthday, sex, levelActivity } =
-    user.userInfo;
+  if (user.userInfo) {
+    const { desiredtWeight, height, birthday, sex, levelActivity } =
+      user.userInfo;
 
-  const bmr = bmrCalculationFn(
-    desiredtWeight,
-    height,
-    birthday,
-    sex,
-    levelActivity
-  );
+    const bmr = bmrCalculationFn(
+      desiredtWeight,
+      height,
+      birthday,
+      sex,
+      levelActivity
+    );
 
-  res.status(200).json({
-    user: {
-      name: user.name,
-      userInfo: user.userInfo,
-    },
-    bmr,
-  });
+    res.status(200).json({
+      user: {
+        name: user.name,
+        userInfo: user.userInfo,
+      },
+      bmr,
+    });
+  } else {
+    res.status(200).json({
+      user: {
+        name: user.name,
+      },
+    });
+  }
 };
 
 module.exports = updateInfo;
