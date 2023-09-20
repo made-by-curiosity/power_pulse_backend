5d04267a5056f27c1929485f7d3342a0b1f99197
TurtlePower <agnkvdm@gmail.com>
2023-09-20 17:03:47 +0300

    update add and get meal controllers

--- controllers/diary/getByDateMeal.js
+++ controllers/diary/getByDateMeal.js
@@ -4,20 +4,23 @@ const { diary } = require('../../models');
 const { meal } = diary;
 
 const getByDateMeal = async (req, res) => {
-	const { _id: owner } = req.user; 
+  const { _id: owner } = req.user;
 
-	const today = new Date();
-	const { date = today } = req.query;
+  const today = new Date();
+  const { date = today } = req.query;
 
-	const listMeal = await meal.Meal.find({
-		owner,
-		createdAt: {
-			$gte: startOfDay(new Date(date)),
-			$lte: endOfDay(new Date(date)),
-		}
-	}, '-owner -createdAt -updatedAt');
+  const listMeal = await meal.Meal.find(
+    {
+      owner,
+      createdAt: {
+        $gte: startOfDay(new Date(date)),
+        $lte: endOfDay(new Date(date)),
+      },
+    },
+    '-owner -createdAt -updatedAt'
+  ).populate('productId', '_id weight calories category title groupBloodNotAllowed');
 
-	res.json(listMeal);
+  res.json(listMeal);
 };
 
 module.exports = getByDateMeal;
--- models/diary/meal.js
+++ models/diary/meal.js
@@ -1,43 +1,46 @@
 const Joi = require('joi');
 const { Schema, model } = require('mongoose');
-const { handleSchemaValidationErrors} = require('../../helpers');
+const { handleSchemaValidationErrors } = require('../../helpers');
 
-const mealSchema = new Schema({
-	name: {
-		type: String,
-		required: [true, 'Set name for product'],
-	},
-	amount: {
-		type: Number,
-		min: [1, 'The amount cannot be less than 1 gram'],
-		required: [true, 'Set the amount of the product'],
-	},
-	calories: {
-		type: Number,
-		min: [1, 'The calories cannot be less than 1'],
-		required: [true, 'Set the calories of the product'],
-	},
-	owner: {
-		type: Schema.Types.ObjectId,
-		required: true,
-		ref: 'user',
-	},
-}, {versionKey: false, timestamps: true});
+const mealSchema = new Schema(
+  {
+    productId: {
+      type: Schema.Types.ObjectId,
+      required: [true, 'Set product ID'],
+    },
+    amount: {
+      type: Number,
+      min: [1, 'The amount cannot be less than 1 gram'],
+      required: [true, 'Set the amount of the product'],
+    },
+    calories: {
+      type: Number,
+      min: [1, 'The calories cannot be less than 1'],
+      required: [true, 'Set the calories of the product'],
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'user',
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
 
 mealSchema.post('save', handleSchemaValidationErrors);
 const Meal = model('meal', mealSchema);
 
 const postMealSchema = Joi.object({
-	name: Joi.string().required(),
-	amount: Joi.number().min(1).required(),
-	calories: Joi.number().min(1).required(),
+  productId: Joi.string().required(),
+  amount: Joi.number().min(1).required(),
+  calories: Joi.number().min(1).required(),
 });
 
 const schemasMeal = {
-	postMealSchema,
+  postMealSchema,
 };
 
 module.exports = {
-	Meal,
-	schemasMeal,
-}
+  Meal,
+  schemasMeal,
+};
