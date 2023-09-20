6867302d46f9d0bcc5cabc4b717bc02028b4c339
TurtlePower <agnkvdm@gmail.com>
2023-09-20 19:19:11 +0300

    update joi validation

--- routes/api/diary.js
+++ routes/api/diary.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const { ctrlDiary } = require('../../controllers');
-const { authenticate, isValidId } = require('../../middlewares');
+const { authenticate, isValidId, validation } = require('../../middlewares');
+const {
+  schemasMeal: { postMealSchema },
+} = require('../../models/diary/meal');
+const {
+  schemasWorkout: { postWorkoutSchema },
+} = require('../../models/diary/workout');
 
 const router = express.Router();
 
 router.get('/meal', authenticate, ctrlDiary.getByDateMeal);
 router.get('/workout', authenticate, ctrlDiary.getByDateWorkout);
-router.post('/meal', authenticate, ctrlDiary.postAddMeal);
-router.post('/workout', authenticate, ctrlDiary.postAddWorkout);
+router.post('/meal', validation(postMealSchema), authenticate, ctrlDiary.postAddMeal);
+router.post('/workout', validation(postWorkoutSchema), authenticate, ctrlDiary.postAddWorkout);
 router.delete('/meal/:id', authenticate, isValidId, ctrlDiary.deleteByIdMeal);
 router.delete('/workout/:id', authenticate, isValidId, ctrlDiary.deleteByIdWorkout);
 
-module.exports = router
+module.exports = router;
