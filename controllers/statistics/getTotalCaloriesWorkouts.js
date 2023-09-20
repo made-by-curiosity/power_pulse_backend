658725b05191ce765363cfe5c4baef9120da7f63
Viktor Biruk <vikttur.job@gmail.com>
2023-09-21 00:17:32 +0300

    Creates statistics controllers
    to get the number of calories burned and the amount of time

--- controllers/statistics/getTotalCaloriesWorkouts.js
+++ controllers/statistics/getTotalCaloriesWorkouts.js
@@ -4,8 +4,8 @@ const { diary } = require('../../models');
 const { workout } = diary;
 
 const getTotalCaloriesWorkouts = async (req, res) => {
-	const totalCaloriesWorkouts = await workout.Workout.countDocuments();
-  res.json(totalCaloriesWorkouts);
+	const totalCaloriesWorkouts = await workout.Workout.aggregate([{ $group: { _id: null, calories: { $sum: "$calories" } } }]);
+  res.json(totalCaloriesWorkouts[0].calories);
 };
 
 module.exports = getTotalCaloriesWorkouts;
--- controllers/statistics/getTotalTimeWorkouts.js
+++ controllers/statistics/getTotalTimeWorkouts.js
@@ -4,8 +4,8 @@ const { diary } = require('../../models');
 const { workout } = diary;
 
 const getTotalTimeWorkouts = async (req, res) => {
-const totalTimeWorkouts = await workout.Workout.countDocuments();
-  res.json(totalTimeWorkouts);
+	const totalTimeWorkouts = await workout.Workout.aggregate([{ $group: { _id: null, time: { $sum: "$time" } } }]);
+  res.json(totalTimeWorkouts[0].time);
 };
 
 module.exports = getTotalTimeWorkouts;
