ebe23f8d1abf5aa2c19e22319e87dd138b1c0d94
Viktor Biruk <vikttur.job@gmail.com>
2023-09-20 18:38:42 +0300

    Creates routes to receive statistics

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const users = require("./users");
 const auth = require("./auth");
 const ctrlDiary = require("./diary");
+const ctrlStatistics = require("./statistics");
 
 module.exports = {
 	users,
   auth,
-  ctrlDiary,
+	ctrlDiary,
+	ctrlStatistics,
 };
---
+++ controllers/statistics/getCountExercises.js
@@ -0,0 +1,26 @@
+const { endOfDay, startOfDay } = require('date-fns');
+const { diary } = require('../../models');
+
+const { meal } = diary;
+
+const getCountExercises = async (req, res) => {
+  const { _id: owner } = req.user;
+
+  const today = new Date();
+  const { date = today } = req.query;
+
+  const listMeal = await meal.Meal.find(
+    {
+      owner,
+      createdAt: {
+        $gte: startOfDay(new Date(date)),
+        $lte: endOfDay(new Date(date)),
+      },
+    },
+    '-owner -createdAt -updatedAt'
+  ).populate('productId', '_id weight calories category title groupBloodNotAllowed');
+
+  res.json(listMeal);
+};
+
+module.exports = getCountExercises;
